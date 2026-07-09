1afd13813c8b90f1f6477df8b8f102e4ffcdf37d
André Gomes <andregs@users.noreply.github.com>
2026-07-09 08:51:07 -0300

    feat(wanderlust-catalog): scaffold spring native skeleton
    Same recipe as wanderlust-bff. Boots, /actuator/health = UP.

---
+++ apps/wanderlust-catalog/src/main/java/br/dev/lean/wanderlust/catalog/WanderlustCatalogApplication.java
@@ -0,0 +1,13 @@
+package br.dev.lean.wanderlust.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderlustCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderlustCatalogApplication.class, args);
+	}
+
+}
---
+++ apps/wanderlust-catalog/src/test/java/br/dev/lean/wanderlust/catalog/WanderlustCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.lean.wanderlust.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderlustCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
