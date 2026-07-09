1c47d4acf24a2e8af27932346b6a2b449b94b2b2
André Gomes <andregs@users.noreply.github.com>
2026-07-09 08:54:58 -0300

    feat(wanderlust-payment): scaffold spring native skeleton
    Same recipe as wanderlust-bff. Boots, /actuator/health = UP.

---
+++ apps/wanderlust-payment/src/test/java/br/dev/lean/wanderlust/payment/WanderlustPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.lean.wanderlust.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderlustPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
